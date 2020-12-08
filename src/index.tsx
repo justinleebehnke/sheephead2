4177a36da664c6352b1e8cc5a50b31755d3ac956
Justin Behnke <justin@databased.com>
2020-12-07 19:02:18 -0700

    Add bootstrap back

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import './Views/index.css'
 import App from './Views/App'
 import reportWebVitals from './reportWebVitals'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(
   <React.StrictMode>
