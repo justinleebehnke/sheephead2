430aeed9b89c7b7848fc2e7d8cdb5717b6895f5c
Justin Behnke <justin@databased.com>
2020-12-25 05:37:01 -0700

    Allow the dealer to be set

--- src/UseCase/GameManager.ts
+++ src/UseCase/GameManager.ts
@@ -4,13 +4,26 @@ import UniqueIdentifier from '../Utilities/UniqueIdentifier'
 class GameManager {
   private host: PlayerDTO
   private players: PlayerDTO[]
+  private firstDealerIndex!: number
   constructor(host: PlayerDTO) {
     this.host = host
     this.players = [host]
+    this.setFirstDealerIndex(0)
   }
 
   public getHost(): PlayerDTO {
     return this.host
+  }
+
+  public setFirstDealerIndex(index: number): void {
+    if (index < 0 || index > 3) {
+      throw Error('First dealer index must be between 0 and 3')
+    }
+    this.firstDealerIndex = index
+  }
+
+  public getFirstDealerIndex(): number {
+    return this.firstDealerIndex
   }
 
   public addPlayer(player: PlayerDTO): void {
--- src/UseCase/TestClasses/GameManager.test.ts
+++ src/UseCase/TestClasses/GameManager.test.ts
@@ -13,11 +13,30 @@ describe('PreGame', () => {
     }).toThrow('Cannot have two players with same id in game')
   })
   it('Should allow the index of the dealer to be set', () => {
-    // const game = new GameManager({ getId: () => hostId, getName: () => 'Host Name' })
-    // // you should be able to mark a pregame as
-    // game.setFirstDealerIndex(1)
+    const game = new GameManager({ getId: () => hostId, getName: () => 'Host Name' })
+    game.setFirstDealerIndex(0)
+    expect(game.getFirstDealerIndex()).toBe(0)
+    game.setFirstDealerIndex(1)
+    expect(game.getFirstDealerIndex()).toBe(1)
+    game.setFirstDealerIndex(2)
+    expect(game.getFirstDealerIndex()).toBe(2)
+    game.setFirstDealerIndex(3)
+    expect(game.getFirstDealerIndex()).toBe(3)
+    expect(() => {
+      game.setFirstDealerIndex(-1)
+    }).toThrow('First dealer index must be between 0 and 3')
+    expect(() => {
+      game.setFirstDealerIndex(4)
+    }).toThrow('First dealer index must be between 0 and 3')
+    expect(() => {
+      game.setFirstDealerIndex(100000000)
+    }).toThrow('First dealer index must be between 0 and 3')
+    expect(() => {
+      game.setFirstDealerIndex(-10000000)
+    }).toThrow('First dealer index must be between 0 and 3')
   })
   it('Should allow a game to be started', () => {})
+  it('Should start the game with CPU players if there aren\'t enough', () => {})
   it('Should not allow a player to be addeed to a started game', () => {})
   it('Should allow a player to be removed from a started game, and bring that game back into a not started state', () => {})
   it('Should allow the host to leave and in that case it should destroy the created game and the pre game', () => {})
