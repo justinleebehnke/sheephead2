92df89935e5cde2ff30dad60fdbeab44edbddb7d
Justin Behnke <justin@databased.com>
2020-12-27 13:50:21 -0700

    Implement bury

--- src/InterfaceAdapters/LocalGameCommandInterface.ts
+++ src/InterfaceAdapters/LocalGameCommandInterface.ts
@@ -1,6 +1,19 @@
 import ICommandInterface from './ICommandInterface'
 import ICommandObject from './ICommandObject'
 import Game from '../Entities/Game'
+import UniqueIdentifier from '../Utilities/UniqueIdentifier'
+const localPlayerId = '79dbc191-2b0e-4dc3-83d7-7696c4abcb61'
+
+interface BuryCommand extends ICommandObject {
+  name: 'bury'
+  params: {
+    cards: string[]
+  }
+}
+
+function isBuryCommand(command: ICommandObject): command is BuryCommand {
+  return command.name === 'bury'
+}
 
 class LocalGameCommandInterface implements ICommandInterface {
   private game: Game
@@ -13,6 +26,12 @@ class LocalGameCommandInterface implements ICommandInterface {
       this.game.getCurrentRound()?.pass()
     } else if (command.name === 'playAgain') {
       this.game.playAnotherRound()
+    } else if (isBuryCommand(command)) {
+      const localPlayer = this.game.getPlayerById(new UniqueIdentifier(localPlayerId))
+      const [cardA, cardB] = command.params.cards
+      this.game
+        .getCurrentRound()
+        ?.bury(localPlayer.removeCardFromHand(cardA), localPlayer.removeCardFromHand(cardB))
     } else {
       throw new Error('Method not implemented.')
     }
