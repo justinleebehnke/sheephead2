42b3d79142c54e45891923b74bc9a71170a698f3
Justin Behnke <justin@databased.com>
2021-02-24 17:56:03 -0700

    Add in a command executor to the game command interface

--- src/InterfaceAdapters/OnlineMultiplayerGameCommandInterface/OnlineMultiplayerGameCommandInterface.test.ts
+++ src/InterfaceAdapters/OnlineMultiplayerGameCommandInterface/OnlineMultiplayerGameCommandInterface.test.ts
@@ -1,3 +1,4 @@
+import ICommandExecutor from '../CommandExecutor/ICommandExecutor'
 import IFetch from '../IFetch'
 import OnlineMultiplayerGameCommandInterface from './OnlineMultiplayerGameCommandInterface'
 import UniqueIdentifier from '../../Utilities/UniqueIdentifier'
@@ -8,16 +9,25 @@ describe('Online Multiplayer Game Command Interface', () => {
   let pollingIntervalInMilliseconds: number
   let fetcher: IFetch
   let hostId: string
+  let commandExecutor: ICommandExecutor
+
   beforeEach(() => {
     hostId = new UniqueIdentifier().getId()
-    baseRoute = 'http://localhost:2020/game/'
+    baseRoute = 'http://localhost:2020/game'
     pollingIntervalInMilliseconds = 100
     fetcher = {
       get: jest
         .fn()
         .mockReturnValueOnce({
           indexOfNextCommand: 15,
-          newCommands: [],
+          newCommands: [{ name: 'Command 1', params: null }],
+        })
+        .mockReturnValueOnce({
+          indexOfNextCommand: 17,
+          newCommands: [
+            { name: 'Command 2', params: null },
+            { name: 'Command 3', params: null },
+          ],
         })
         .mockReturnValue({
           indexOfNextCommand: 17,
@@ -25,24 +35,30 @@ describe('Online Multiplayer Game Command Interface', () => {
         }),
       post: jest.fn(),
     }
+    commandExecutor = {
+      execute: jest.fn(),
+    }
     new OnlineMultiplayerGameCommandInterface(
       pollingIntervalInMilliseconds,
       fetcher,
       baseRoute,
-      hostId
+      hostId,
+      commandExecutor
     )
   })
+
   it('Should poll the server for any new commands according to the interval it was constructed with', async () => {
+    const halfInterval = pollingIntervalInMilliseconds / 2
     expect(fetcher.get).toHaveBeenCalledTimes(1)
-    await pause(pollingIntervalInMilliseconds / 2)
+    await pause(halfInterval)
     expect(fetcher.get).toHaveBeenCalledTimes(1)
-    await pause(pollingIntervalInMilliseconds / 2)
+    await pause(halfInterval)
     expect(fetcher.get).toHaveBeenCalledTimes(2)
-    await pause(pollingIntervalInMilliseconds / 2)
+    await pause(halfInterval)
     expect(fetcher.get).toHaveBeenCalledTimes(2)
-    await pause(pollingIntervalInMilliseconds / 2)
+    await pause(halfInterval)
     expect(fetcher.get).toHaveBeenCalledTimes(3)
-    await pause(pollingIntervalInMilliseconds * 5)
+    await pause(pollingIntervalInMilliseconds * 5 + halfInterval) // this half interval is padding
     expect(fetcher.get).toHaveBeenCalledTimes(8)
   })
 
@@ -59,7 +75,16 @@ describe('Online Multiplayer Game Command Interface', () => {
     await pause(pollingIntervalInMilliseconds)
   })
 
-  // TODO should give the commands over to a command executor
+  it('Should give only the new commands that come back from each response over to his command executor', async () => {
+    expect(commandExecutor.execute).toHaveBeenNthCalledWith(1, { name: 'Command 1', params: null })
+    await pause(pollingIntervalInMilliseconds)
+    expect(commandExecutor.execute).toHaveBeenNthCalledWith(2, { name: 'Command 2', params: null })
+    expect(commandExecutor.execute).toHaveBeenNthCalledWith(3, { name: 'Command 3', params: null })
+    await pause(pollingIntervalInMilliseconds)
+    await pause(pollingIntervalInMilliseconds)
+    expect(commandExecutor.execute).toHaveBeenCalledTimes(3)
+  })
+
   // TODO that command executor should be the same one that the local system uses
   // TODO when given a command it should post and then update the response the same way that it does when given one.
   // TODO while waiting for a response to come back from polling it should not be able to get into a situation where it asks for the same POLL twice before a poll can answer back, so basically we want to create a test where the fetch response takes longer than a poll would normally take, just to make sure he is handling that well.
--- src/InterfaceAdapters/OnlineMultiplayerGameCommandInterface/OnlineMultiplayerGameCommandInterface.ts
+++ src/InterfaceAdapters/OnlineMultiplayerGameCommandInterface/OnlineMultiplayerGameCommandInterface.ts
@@ -1,4 +1,5 @@
 import ICommandCommunicatorResponse from '../ICommandCommunicatorResponse'
+import ICommandExecutor from '../CommandExecutor/ICommandExecutor'
 import ICommandInterface from '../ICommandInterface'
 import ICommandObject from '../ICommandObject'
 import IFetch from '../IFetch'
@@ -8,26 +9,36 @@ class OnlineMultiplayerGameCommandInterface implements ICommandInterface {
   private readonly fetcher: IFetch
   private readonly baseRoute: string
   private readonly hostId: string
+  private readonly commandExecutor: ICommandExecutor
   private indexOfNextCommand: number
 
-  constructor(pollingFrequency: number, fetcher: IFetch, baseRoute: string, hostId: string) {
+  constructor(
+    pollingFrequency: number,
+    fetcher: IFetch,
+    baseRoute: string,
+    hostId: string,
+    commandExecutor: ICommandExecutor
+  ) {
     this.pollingFrequency = pollingFrequency
     this.fetcher = fetcher
     this.baseRoute = baseRoute
     this.hostId = hostId
     this.indexOfNextCommand = 0
     this.getCommands()
+    this.commandExecutor = commandExecutor
   }
 
   private async getCommands(): Promise<void> {
     const response = await this.fetcher.get(
       `${this.baseRoute}/${this.hostId}/${this.indexOfNextCommand}`
     )
-    if (
-      this.isCommandCommunicatorResponse(response) &&
-      response.indexOfNextCommand > this.indexOfNextCommand
-    ) {
-      this.indexOfNextCommand = response.indexOfNextCommand
+    if (this.isCommandCommunicatorResponse(response)) {
+      if (response.indexOfNextCommand > this.indexOfNextCommand) {
+        this.indexOfNextCommand = response.indexOfNextCommand
+      }
+      response.newCommands.forEach((newCommand: ICommandObject) =>
+        this.commandExecutor.execute(newCommand)
+      )
     }
     setTimeout(() => this.getCommands(), this.pollingFrequency)
   }
