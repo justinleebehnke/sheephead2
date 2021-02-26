72c7f92744435829669fdd12fd4dc33d2abe4bb8
Justin Behnke <justin@databased.com>
2021-02-25 18:58:26 -0700

    Implement executable PlayCommand

---
+++ src/InterfaceAdapters/CommandExecutor/GameCommands/PlayCommand.ts
@@ -0,0 +1,26 @@
+import ICommand from './ICommand'
+import IGame from '../Interfaces/IGame'
+
+class PlayCommand implements ICommand {
+  private readonly game: IGame
+  private readonly cardId: string
+
+  constructor(game: IGame, cardId: string) {
+    this.game = game
+    this.cardId = cardId
+  }
+
+  public execute(): void {
+    const round = this.game.getCurrentRound()
+    if (!round) {
+      throw Error('Cannot play because there is no round')
+    }
+    const player = round.getCurrentTurnPlayer()
+    if (!player) {
+      throw Error("Cannot play because it is not anyone's turn")
+    }
+    round.play(player.removeCardFromHand(this.cardId))
+  }
+}
+
+export default PlayCommand
