4b9d0f4556559bd77345dda436dc2788ad23f497
Justin Behnke <justin@databased.com>
2021-02-27 16:41:25 -0700

    Delete all of the files that I am not using.
    Deleting this files did not impact the game play at all.
    And all tests continue to pass

--- src/Drivers/ServerCommunicator.ts
+++
@@ -1,3 +0,0 @@
-class ServerCommunicator {}
-
-export default ServerCommunicator
--- src/InterfaceAdapters/LobbyCommandInterface.test.ts
+++
@@ -1,9 +0,0 @@
-describe('Lobby Command Interface', () => {
-  it('Should accept a new hosted game command', () => {
-
-    // so newly hosted game command... what is that exactly???
-
-  })
-})
-
-export {}
