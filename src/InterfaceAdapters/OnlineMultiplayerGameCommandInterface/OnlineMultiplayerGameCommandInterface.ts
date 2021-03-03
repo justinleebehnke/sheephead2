ae85178cf0d7f43fd523443f05dee6b2f21c5e70
Justin Behnke <justin@databased.com>
2021-03-02 17:40:01 -0700

    Remove duplicated code from Command Interface

--- src/InterfaceAdapters/OnlineMultiplayerGameCommandInterface/OnlineMultiplayerGameCommandInterface.ts
+++ src/InterfaceAdapters/OnlineMultiplayerGameCommandInterface/OnlineMultiplayerGameCommandInterface.ts
@@ -1,54 +1,16 @@
-import CommandDTO from '../CommandExecutor/CommandDTO'
-import ICommandCommunicatorResponse from '../ICommandCommunicatorResponse'
 import ICommandExecutor from '../CommandExecutor/ICommandExecutor'
-import ICommandInterface from '../ICommandInterface'
 import IFetch from '../IFetch'
+import LobbyCommandInterface from '../LobbyCommandInterface/LobbyCommandInterface'
 
-class OnlineMultiplayerGameCommandInterface implements ICommandInterface {
-  private indexOfNextCommand: number
-
+class OnlineMultiplayerGameCommandInterface extends LobbyCommandInterface {
   constructor(
-    private readonly pollingFrequency: number,
-    private readonly fetcher: IFetch,
-    private readonly baseRoute: string,
-    private readonly hostId: string,
-    private readonly commandExecutor: ICommandExecutor
+    pollingFrequency: number,
+    fetcher: IFetch,
+    baseRoute: string,
+    hostId: string,
+    commandExecutor: ICommandExecutor
   ) {
-    this.indexOfNextCommand = 0
-    this.getCommands()
-  }
-
-  private async getCommands(): Promise<void> {
-    const response = await this.fetcher.get(
-      `${this.baseRoute}/${this.hostId}/${this.indexOfNextCommand}`
-    )
-    this.handleResponse(response)
-    setTimeout(() => this.getCommands(), this.pollingFrequency)
-  }
-
-  public async giveCommand(command: CommandDTO): Promise<void> {
-    this.fetcher.post(`${this.baseRoute}/${this.hostId}`, command)
-  }
-
-  private handleResponse(response: object): void {
-    if (this.isCommandCommunicatorResponse(response)) {
-      if (response.indexOfNextCommand > this.indexOfNextCommand) {
-        this.indexOfNextCommand = response.indexOfNextCommand
-      }
-      response.newCommands.forEach((newCommand: CommandDTO) =>
-        this.commandExecutor.execute(newCommand)
-      )
-    }
-  }
-
-  private isCommandCommunicatorResponse(
-    response: object
-  ): response is ICommandCommunicatorResponse {
-    return (
-      response &&
-      Object.prototype.hasOwnProperty.call(response, 'indexOfNextCommand') &&
-      Object.prototype.hasOwnProperty.call(response, 'newCommands')
-    )
+    super(pollingFrequency, fetcher, `${baseRoute}/${hostId}`, commandExecutor)
   }
 }
 
