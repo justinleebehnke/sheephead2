7b7a6b8991d2a647525bbe707010dc4a5aa1e391
Justin Behnke <justin@databased.com>
2021-03-07 05:51:08 -0700

    Implement isHosting

--- src/Views/GameLobbyViews/PreGameView/PreGamePresenter.test.ts
+++ src/Views/GameLobbyViews/PreGameView/PreGamePresenter.test.ts
@@ -1,8 +1,9 @@
 import GameData from '../../../Entities/GameManager/GameData'
-import ISubscriber from '../../../Entities/ISubscriber'
-import UniqueIdentifier from '../../../Utilities/UniqueIdentifier'
 import IGameList from '../JoinableGamesView/IGameList'
+import ILocalPlayerInfoManager from '../LobbyEntranceView/ILocalPlayerInfoManager'
+import ISubscriber from '../../../Entities/ISubscriber'
 import PreGamePresenter from './PreGamePresenter'
+import UniqueIdentifier from '../../../Utilities/UniqueIdentifier'
 
 describe('PreGamePresenter', () => {
   let presenter: PreGamePresenter
@@ -10,7 +11,17 @@ describe('PreGamePresenter', () => {
   let gameList: IGameList
   let hostId: UniqueIdentifier
   let game: GameData
+  let localPlayerInfoManager: ILocalPlayerInfoManager
+  let localPlayerId: UniqueIdentifier
+
   beforeEach(() => {
+    localPlayerId = new UniqueIdentifier()
+    localPlayerInfoManager = {
+      getPlayerId: () => localPlayerId.getId(),
+      getPlayerName: () => 'Butch Coolidge',
+      setPlayerId: () => {},
+      setPlayerName: () => {},
+    }
     hostId = new UniqueIdentifier()
     game = {
       isStarted: false,
@@ -29,8 +40,9 @@ describe('PreGamePresenter', () => {
       getAllGames: jest.fn(),
       getGameByHostId: jest.fn().mockReturnValue(game),
     }
-    presenter = new PreGamePresenter(gameList, hostId)
+    presenter = new PreGamePresenter(gameList, hostId, localPlayerInfoManager)
   })
+
   describe('setView', () => {
     it('Should subscribe to the game list, and notify the view if the game list updates', () => {
       presenter.setView(view)
@@ -39,20 +51,32 @@ describe('PreGamePresenter', () => {
       expect(view.update).toHaveBeenCalled()
     })
   })
+
   describe('isHosting', () => {
-    // if the local player is the host, then the local player is hosting
-    // otherwise they are not
-    // when you are hosting you can remove players
-    // when you are hosting you can set the first dealer index
-    // when you are hosting you can start the game
+    it('Should return true if the local player is the host', () => {
+      presenter = new PreGamePresenter(gameList, localPlayerId, localPlayerInfoManager)
+      expect(presenter.isHosting()).toBe(true)
+    })
+    it('Should return false if the local player is not the host', () => {
+      presenter = new PreGamePresenter(gameList, hostId, localPlayerInfoManager)
+      expect(presenter.isHosting()).toBe(false)
+    })
   })
-  describe('getFirstDealerIndex', () => {})
+
+  describe('getFirstDealerIndex', () => {
+    // this would make more sense if it said somethinkg like "Random" | "Player 1" etc, rather than talk of numbers
+    // when the game command is sent it will be easier to just to the right thing
+  })
+
   describe('getPlayers', () => {
     it('Should return the player data for the game that is being viewed', () => {
       expect(presenter.getPlayers()).toEqual([{ id: hostId.getId(), name: 'Marsellus Wallace' }])
     })
   })
 
+  // when you are hosting you can remove players
+  // when you are hosting you can set the first dealer index
+  // when you are hosting you can start the game
   describe('setFirstDealerIndex', () => {
     // -1 is a code word for random, when the game is actually created
   })
@@ -60,6 +84,7 @@ describe('PreGamePresenter', () => {
   describe('removePlayerFromGame', () => {
     // throw error if not host
   })
+
   describe('leaveGame', () => {
     // send a leave game ocmmand
   })
--- src/Views/GameLobbyViews/PreGameView/PreGamePresenter.ts
+++ src/Views/GameLobbyViews/PreGameView/PreGamePresenter.ts
@@ -1,14 +1,19 @@
-import ISubscriber from '../../../Entities/ISubscriber'
-import PlayerDTO from '../../../UseCase/PlayerDTO'
-import UniqueIdentifier from '../../../Utilities/UniqueIdentifier'
-import PlayerData from '../../GamePlayViews/EndOfRoundReport/PlayerData'
 import IGameList from '../JoinableGamesView/IGameList'
 import IGameListSubscriber from '../JoinableGamesView/IGameListSubscriber'
+import ILocalPlayerInfoManager from '../LobbyEntranceView/ILocalPlayerInfoManager'
+import ISubscriber from '../../../Entities/ISubscriber'
+import PlayerData from '../../GamePlayViews/EndOfRoundReport/PlayerData'
+import PlayerDTO from '../../../UseCase/PlayerDTO'
+import UniqueIdentifier from '../../../Utilities/UniqueIdentifier'
 
 class PreGamePresenter implements IGameListSubscriber {
   private view: ISubscriber | undefined
 
-  constructor(private readonly gameList: IGameList, private readonly hostId: UniqueIdentifier) {
+  constructor(
+    private readonly gameList: IGameList,
+    private readonly hostId: UniqueIdentifier,
+    private readonly localPlayerInfoManager: ILocalPlayerInfoManager
+  ) {
     this.gameList.subscribe(this)
   }
 
@@ -24,6 +29,10 @@ class PreGamePresenter implements IGameListSubscriber {
     return this.gameList.getGameByHostId(this.hostId).players.map((playerData: PlayerDTO) => {
       return { id: playerData.id.getId(), name: playerData.name }
     })
+  }
+
+  public isHosting(): boolean {
+    return this.hostId.equals(new UniqueIdentifier(this.localPlayerInfoManager.getPlayerId()))
   }
 }
 
