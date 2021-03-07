d82118f0aab06cbf15f939bfb8c2372e3c4ce155
Justin Behnke <justin@databased.com>
2021-03-07 06:32:43 -0700

    Implement remove player logic

--- src/Views/GameLobbyViews/PreGameView/PreGamePresenter.test.ts
+++ src/Views/GameLobbyViews/PreGameView/PreGamePresenter.test.ts
@@ -1,8 +1,10 @@
 import GameData from '../../../Entities/GameManager/GameData'
+import ICommandInterface from '../../../InterfaceAdapters/ICommandInterface'
 import IGameList from '../JoinableGamesView/IGameList'
 import ILocalPlayerInfoManager from '../LobbyEntranceView/ILocalPlayerInfoManager'
 import ISubscriber from '../../../Entities/ISubscriber'
 import PreGamePresenter from './PreGamePresenter'
+import RemovePlayerFromGameCommandDTO from '../../../InterfaceAdapters/CommandExecutor/LobbyCommands/LobbyCommandDTOs/RemovePlayerFromGameCommandDTO'
 import UniqueIdentifier from '../../../Utilities/UniqueIdentifier'
 
 describe('PreGamePresenter', () => {
@@ -13,8 +15,13 @@ describe('PreGamePresenter', () => {
   let game: GameData
   let localPlayerInfoManager: ILocalPlayerInfoManager
   let localPlayerId: UniqueIdentifier
+  let otherPlayerId: UniqueIdentifier
+  let commandInterface: ICommandInterface
 
   beforeEach(() => {
+    commandInterface = {
+      giveCommand: jest.fn(),
+    }
     localPlayerId = new UniqueIdentifier()
     localPlayerInfoManager = {
       getPlayerId: () => localPlayerId.getId(),
@@ -22,6 +29,7 @@ describe('PreGamePresenter', () => {
       setPlayerId: () => {},
       setPlayerName: () => {},
     }
+    otherPlayerId = new UniqueIdentifier()
     hostId = new UniqueIdentifier()
     game = {
       isStarted: false,
@@ -40,7 +48,7 @@ describe('PreGamePresenter', () => {
       getAllGames: jest.fn(),
       getGameByHostId: jest.fn().mockReturnValue(game),
     }
-    presenter = new PreGamePresenter(gameList, hostId, localPlayerInfoManager)
+    presenter = new PreGamePresenter(gameList, hostId, localPlayerInfoManager, commandInterface)
   })
 
   describe('setView', () => {
@@ -54,18 +62,30 @@ describe('PreGamePresenter', () => {
 
   describe('isHosting', () => {
     it('Should return true if the local player is the host', () => {
-      presenter = new PreGamePresenter(gameList, localPlayerId, localPlayerInfoManager)
+      presenter = new PreGamePresenter(
+        gameList,
+        localPlayerId,
+        localPlayerInfoManager,
+        commandInterface
+      )
       expect(presenter.isHosting()).toBe(true)
     })
     it('Should return false if the local player is not the host', () => {
-      presenter = new PreGamePresenter(gameList, hostId, localPlayerInfoManager)
+      presenter = new PreGamePresenter(gameList, hostId, localPlayerInfoManager, commandInterface)
       expect(presenter.isHosting()).toBe(false)
     })
   })
 
-  describe('getFirstDealerIndex', () => {
-    // this would make more sense if it said somethinkg like "Random" | "Player 1" etc, rather than talk of numbers
-    // when the game command is sent it will be easier to just to the right thing
+  describe('Get Dealer Select Values', () => {
+    it('Should return the appropriate values in the appropriate order', () => {
+      expect(presenter.getDealerSelectDropDownData()).toEqual([
+        { value: -1, displayedValue: 'Random' },
+        { value: 0, displayedValue: 'Host (You)' },
+        { value: 1, displayedValue: 'Player 2' },
+        { value: 2, displayedValue: 'Player 3' },
+        { value: 3, displayedValue: 'Player 4' },
+      ])
+    })
   })
 
   describe('getPlayers', () => {
@@ -74,22 +94,79 @@ describe('PreGamePresenter', () => {
     })
   })
 
-  // when you are hosting you can remove players
-  // when you are hosting you can set the first dealer index
-  // when you are hosting you can start the game
   describe('setFirstDealerIndex', () => {
-    // -1 is a code word for random, when the game is actually created
+    it('Should throw an error if someone other than the host tries to do this', () => {
+      expect(() => presenter.setFirstDealerIndex(2)).toThrow(
+        'Only the host may set the dealer index'
+      )
+    })
+    it('Should update immediately and notify the view that the value has changed', () => {
+      presenter = new PreGamePresenter(
+        gameList,
+        localPlayerId,
+        localPlayerInfoManager,
+        commandInterface
+      )
+      presenter.setView(view)
+      presenter.setFirstDealerIndex(2)
+      expect(view.update).toHaveBeenCalled()
+      expect(presenter.getFirstDealerIndex()).toBe(2)
+    })
+    it('Should throw an error if the value is too big or too small', () => {
+      presenter = new PreGamePresenter(
+        gameList,
+        localPlayerId,
+        localPlayerInfoManager,
+        commandInterface
+      )
+      expect(() => presenter.setFirstDealerIndex(-2)).toThrow('Value must be between -1 and 3')
+      expect(() => presenter.setFirstDealerIndex(4)).toThrow('Value must be between -1 and 3')
+    })
   })
-  // throw error if not host
+
+  describe('getFirstDealerIndex', () => {
+    it('Should throw an error if someone other than the host is asking', () => {
+      expect(presenter.isHosting()).toBe(false)
+      expect(() => presenter.getFirstDealerIndex()).toThrow(
+        'Only the host may read the index of the first dealer'
+      ) // this will likely be changed in the future when a 'configUpdate' lobby command is made
+    })
+  })
+
   describe('removePlayerFromGame', () => {
-    // throw error if not host
+    it('Should throw an error if the local player is not the host', () => {
+      expect(() => presenter.removePlayer(otherPlayerId.getId())).toThrow(
+        'Only the host may remove a player'
+      )
+    })
+    it('Should send a remove player from game command', () => {
+      const removePlayerCommand: RemovePlayerFromGameCommandDTO = {
+        name: 'removePlayer',
+        params: {
+          hostId: hostId.getId(),
+          playerId: otherPlayerId.getId(),
+        },
+      }
+
+      presenter = new PreGamePresenter(
+        gameList,
+        localPlayerId,
+        localPlayerInfoManager,
+        commandInterface
+      )
+
+      presenter.removePlayer(otherPlayerId.getId())
+      expect(commandInterface.giveCommand).toHaveBeenCalledWith(removePlayerCommand)
+    })
   })
 
   describe('leaveGame', () => {
     // send a leave game ocmmand
+    // it should create a removePlayerFromGame command with the local player info
   })
 
   describe('startGame', () => {
+    // when you are hosting you can start the game
     // throw error if not host
     // throw error if there are not 4 players
     // if the current index is -1 choose a random index before sending the command (do not send -1) everyone else needs to know who the real first dealer is
--- src/Views/GameLobbyViews/PreGameView/PreGamePresenter.ts
+++ src/Views/GameLobbyViews/PreGameView/PreGamePresenter.ts
@@ -1,3 +1,4 @@
+import ICommandInterface from '../../../InterfaceAdapters/ICommandInterface'
 import IGameList from '../JoinableGamesView/IGameList'
 import IGameListSubscriber from '../JoinableGamesView/IGameListSubscriber'
 import ILocalPlayerInfoManager from '../LobbyEntranceView/ILocalPlayerInfoManager'
@@ -5,16 +6,20 @@ import ISubscriber from '../../../Entities/ISubscriber'
 import PlayerData from '../../GamePlayViews/EndOfRoundReport/PlayerData'
 import PlayerDTO from '../../../UseCase/PlayerDTO'
 import UniqueIdentifier from '../../../Utilities/UniqueIdentifier'
+import RemovePlayerFromGameCommandDTO from '../../../InterfaceAdapters/CommandExecutor/LobbyCommands/LobbyCommandDTOs/RemovePlayerFromGameCommandDTO'
 
 class PreGamePresenter implements IGameListSubscriber {
   private view: ISubscriber | undefined
+  private firstDealerIndex: number
 
   constructor(
     private readonly gameList: IGameList,
     private readonly hostId: UniqueIdentifier,
-    private readonly localPlayerInfoManager: ILocalPlayerInfoManager
+    private readonly localPlayerInfoManager: ILocalPlayerInfoManager,
+    private readonly commandInterface: ICommandInterface
   ) {
     this.gameList.subscribe(this)
+    this.firstDealerIndex = -1
   }
 
   public gameListUpdated(): void {
@@ -33,6 +38,50 @@ class PreGamePresenter implements IGameListSubscriber {
 
   public isHosting(): boolean {
     return this.hostId.equals(new UniqueIdentifier(this.localPlayerInfoManager.getPlayerId()))
+  }
+
+  public getDealerSelectDropDownData(): { value: number; displayedValue: string }[] {
+    return [
+      { value: -1, displayedValue: 'Random' },
+      { value: 0, displayedValue: 'Host (You)' },
+      { value: 1, displayedValue: 'Player 2' },
+      { value: 2, displayedValue: 'Player 3' },
+      { value: 3, displayedValue: 'Player 4' },
+    ]
+  }
+
+  public setFirstDealerIndex(index: number): void {
+    if (!this.isHosting()) {
+      throw Error('Only the host may set the dealer index')
+    }
+    if (index < -1 || index > 3) {
+      throw Error('Value must be between -1 and 3')
+    }
+    this.firstDealerIndex = index
+    this.view?.update()
+  }
+
+  public getFirstDealerIndex(): number {
+    if (!this.isHosting()) {
+      throw Error('Only the host may read the index of the first dealer')
+    }
+    return this.firstDealerIndex
+  }
+
+  public removePlayer(playerId: string): any {
+    if (!this.isHosting) {
+      throw new Error('Only the host may remove a player')
+    }
+
+    const removePlayerCommand: RemovePlayerFromGameCommandDTO = {
+      name: 'removePlayer',
+      params: {
+        hostId: this.hostId.getId(),
+        playerId,
+      },
+    }
+
+    this.commandInterface.giveCommand(removePlayerCommand)
   }
 }
 
