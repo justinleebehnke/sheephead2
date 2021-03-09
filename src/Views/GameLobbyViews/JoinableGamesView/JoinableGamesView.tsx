35049129e6a8ba2f7c17f77c104688d01b175341
Justin Behnke <justin@databased.com>
2021-03-08 19:20:50 -0700

    Implement joinable games table

--- src/Views/GameLobbyViews/JoinableGamesView/JoinableGamesView.tsx
+++ src/Views/GameLobbyViews/JoinableGamesView/JoinableGamesView.tsx
@@ -1,10 +1,15 @@
 import { Component, ReactElement } from 'react'
+import Button from 'react-bootstrap/Button'
+import Table from 'react-bootstrap/Table'
 import IJoinableGamesPresenter from './IJoinableGamesPresenter'
 import ISubscriber from '../../../Entities/ISubscriber'
+import JoinableGameData from './JoinableGameData'
 
 type Props = {
   presenter: IJoinableGamesPresenter
 }
+
+const NUM_PLAYERS_IN_GAME = 4
 
 class JoinableGamesView extends Component<Props> implements ISubscriber {
   componentDidMount(): void {
@@ -12,7 +17,50 @@ class JoinableGamesView extends Component<Props> implements ISubscriber {
   }
 
   render(): ReactElement {
-    return <div>I AM THE GAME LIST</div>
+    return (
+      <div>
+        <h4 className='joinable-games-header'>Here are the games you can join</h4>
+        <div className='wrap'>
+          <Table bordered hover variant='dark'>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Host</th>
+                <th>Player 2</th>
+                <th>Player 3</th>
+                <th>Player 4</th>
+                <th>Join</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.props.presenter
+                .getJoinableGameData()
+                .map((game: JoinableGameData, index: number) => {
+                  return (
+                    <tr key={game.hostId.getId()}>
+                      <td>{index + 1}</td>
+                      {game.playerNames.map((playerName: string) => {
+                        return <td key={playerName}>{playerName}</td>
+                      })}
+                      {new Array(NUM_PLAYERS_IN_GAME - game.playerNames.length).fill(0).map(() => (
+                        <td></td>
+                      ))}
+                      <td>
+                        <Button
+                          variant='primary'
+                          onClick={() => this.props.presenter.joinGame(game.hostId.getId())}
+                        >
+                          Join
+                        </Button>
+                      </td>
+                    </tr>
+                  )
+                })}
+            </tbody>
+          </Table>
+        </div>
+      </div>
+    )
   }
 
   update = (): void => {
