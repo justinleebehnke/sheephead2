73a342b5ff49c0280a235705a4c42ead676d10d1
Justin Behnke <justin@databased.com>
2021-03-14 19:53:40 -0600

    Clean up the styles

--- src/Views/GameLobbyViews/LobbyEntranceView/LobbyEntranceView.tsx
+++ src/Views/GameLobbyViews/LobbyEntranceView/LobbyEntranceView.tsx
@@ -18,22 +18,24 @@ class LobbyEntranceView extends Component<Props> {
 
   render(): ReactElement {
     return (
-      <div id='lobby-entrance'>
-        <h1>Game Lobby</h1>
-        <div>
-          <InputGroup size='lg'>
-            <InputGroup.Prepend>
-              <InputGroup.Text id='inputGroup-sizing-lg'>Your Displayed Name</InputGroup.Text>
-            </InputGroup.Prepend>
-            <FormControl
-              required
-              onBlur={() => this.props.presenter.nameInputBlurred()}
-              onChange={(event) => this.updateName(event)}
-              aria-label='Large'
-              aria-describedby='inputGroup-sizing-sm'
-              value={this.props.presenter.getLocalPlayerName()}
-            />
-          </InputGroup>
+      <div id='lobby-entrance' className='lobby'>
+        <div className='split'>
+          <h1>Game Lobby</h1>
+          <div>
+            <InputGroup size='lg'>
+              <InputGroup.Prepend>
+                <InputGroup.Text id='inputGroup-sizing-lg'>Your Displayed Name</InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl
+                required
+                onBlur={() => this.props.presenter.nameInputBlurred()}
+                onChange={(event) => this.updateName(event)}
+                aria-label='Large'
+                aria-describedby='inputGroup-sizing-sm'
+                value={this.props.presenter.getLocalPlayerName()}
+              />
+            </InputGroup>
+          </div>
         </div>
         <Button onClick={() => this.props.presenter.hostNewGame()} id='host-new-game-button'>
           Host a new Game
